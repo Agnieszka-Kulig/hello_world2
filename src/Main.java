02296ce095b56a13ada95e74913271339ef963e8
Agusia <aga.gasiewicz@gmail.com>
2020-11-01 23:00:23 +0100

    nauka2

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main{
+
+    public static void main(String[] args) {
+        int tab[] = new int[5];
+        tab[0] = 6;
+        tab[1] = 6;
+        tab[2] = 3;
+        tab[3] = 5;
+        tab[4] = 9;
+        for (int i = 0; i < 5; i++)
+        {
+            System.out.println("Element tablicy = " + tab[i]);
+        }
+    }
+
+}
+
